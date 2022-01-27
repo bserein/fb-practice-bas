6a9126e0004c0426234115cb7b494855964f72be
Brian S <brians@Brians-MacBook-Air.local>
2022-01-27 12:07:04 -0500

    connected to firestore and added a restaurant

--- index.js
+++ index.js
@@ -6,9 +6,27 @@ const { initializeApp, applicationDefault, cert } = require('firebase-admin/app'
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
 //console.log(restaurants[0].name) // do this just to check to see if it worked
-//connect to firestore
+
 //connect private key and place in .gitignore to make sure it doesnt show up in the public repository
-const credentials = require('./credentials.json')
+// import our credentials 
+const credentials = require('./credentials.json');
+
+// connect to firebase services
+initializeApp({
+    credential: cert (credentials)
+});
+
+//connect to firestore
+const db = getFirestore();
+
 // create a collection caled "restaurants"
 
+
 // add each restaurant
+db.collection('restaurants').add(restaurants[3]) // this will go to the data base creating a document of the restaurant
+.then(doc => {
+    console.log('Added restaurant', doc.id)//this will create a unique ID like in mongo
+})
+.catch(err => {
+    console.error(err); // this will tell us what the error is if one shows up
+});
