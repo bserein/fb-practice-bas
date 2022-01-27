9b700ec2a8ae1ad88d567da54408d9d7870d0d8d
Brian S <brians@Brians-MacBook-Air.local>
2022-01-27 11:45:10 -0500

    added firebase admin and credentials

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // import our restaurants
 const restaurants = require('./restaurants.json');// doing ./ will load out the json file thats in the same folder
 
-console.log(restaurants[0].name)
-//connect to firestore
+//import a set of tools to talk to firebase and firestore
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app'); // get this from the firebase documentation
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
+//console.log(restaurants[0].name) // do this just to check to see if it worked
+//connect to firestore
+//connect private key and place in .gitignore to make sure it doesnt show up in the public repository
+const credentials = require('./credentials.json')
 // create a collection caled "restaurants"
 
 // add each restaurant
