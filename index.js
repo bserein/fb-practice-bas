8f041153046a88887b5db85a46b09088c2187fed
Brian S <brians@Brians-MacBook-Air.local>
2022-01-27 14:29:47 -0500

    finished querying data

--- index.js
+++ index.js
@@ -20,13 +20,39 @@ initializeApp({
 const db = getFirestore();
 
 // create a collection caled "restaurants"
-
+const restRef = db.collection('restaurants') // shortcut to not have to type a lot
 
 // add each restaurant
-db.collection('restaurants').add(restaurants[3]) // this will go to the data base creating a document of the restaurant
-.then(doc => {
-    console.log('Added restaurant', doc.id)//this will create a unique ID like in mongo
+// db.collection('restaurants').add(restaurants[3]) // this will go to the data base creating a document of the restaurant
+// .then(doc => {
+//     console.log('Added restaurant', doc.id)//this will create a unique ID like in mongo
+// })
+// .catch(err => {
+//     console.error(err); // this will tell us what the error is if one shows up
+// });
+
+
+// how to read one document
+// db.collection('restaurants').doc('JM1XdWEc8EklqcLtlqV0').get()
+// .then(doc => {
+//     console.log(doc.id, ' => ', doc.data()); //.data returns data from a document 
+// })
+// .catch(err => console.error(err));
+
+// get all documents
+// db.collection('restaurants').get()
+// .then(snapshot => {
+//     snapshot.forEach(doc => {
+//         console.log(doc.id, ' => ', doc.data())
+//     })
+// })
+// .catch(console.error)
+
+// find a document(s) also known as querying a collection
+restRef.where('name', '==', 'Boley').get() // you filter then you get it
+.then(snapshot => {
+    snapshot.forEach(doc => {
+        console.log(doc.data());
+    });
 })
-.catch(err => {
-    console.error(err); // this will tell us what the error is if one shows up
-});
+.catch(console.error);
